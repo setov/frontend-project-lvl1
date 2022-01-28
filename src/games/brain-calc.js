b8c39449013ceaa060cc0761f3cd76554ec0c675
Sergey Titov <ya.serge-vladimirov@yandex.ru>
2022-01-28 04:25:39 +0300

    rewrite brain-calc.js. add function getCalculationResult

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -10,6 +10,20 @@ const EXPRESSIONS = {
   '+': (a, b) => a + b,
   '*': (a, b) => a * b,
 };
+
+/**
+ * get arithmetical result of the expression.
+ * @param {Number} operand1 first operand
+ * @param {Number} operand2 second operand
+ * @param {String} operation operation
+ * @returns {String[]} [question, answer]
+ */
+const getCalculationResult = (operand1, operand2, operation) => {
+  const answer = String(EXPRESSIONS[operation](operand1, operand2));
+  const question = `${operand1} ${operation} ${operand2}`;
+  return [question, answer];
+};
+
 /**
  * every call function from this module
  * generate new random question, answer data.
@@ -21,10 +35,9 @@ const generate = () => {
 
   const randomIndex = Math.floor(Math.random() * OPERATIONS.length);
   const operation = OPERATIONS[randomIndex];
-  const answer = String(EXPRESSIONS[operation](a, b));
-  const question = `${a} ${operation} ${b}`;
+  const data = getCalculationResult(a, b, operation);
 
-  return [question, answer];
+  return data;
 };
 
 export default () => run(DESCRIPTION, generate);
